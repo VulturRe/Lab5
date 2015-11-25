221cf0155941b27c375f8162b5e14c8ae9cb47e6
Ilshat Galimov <einnks@gmail.com>
2015-11-25 22:22:12 +0300

    Lab5 is done

--- Lab5/SetOfChar.cs
+++ Lab5/SetOfChar.cs
@@ -8,6 +8,11 @@ namespace Lab5
     {
         protected List<char> CharList { get; }
 
+        public SetOfChar()
+        {
+           CharList = new List<char>();
+        }
+
         public SetOfChar(char ch)
         {
             CharList = new List<char> {ch};
@@ -27,25 +32,55 @@ namespace Lab5
 
         public static SetOfChar operator +(SetOfChar set, char ch)
         {
-            set.Add(ch);
-            return set;
+            var set1 = set;
+            set1.Add(ch);
+            return set1;
         }
 
         public static SetOfChar operator +(SetOfChar set1, SetOfChar set2)
         {
-            foreach (var c in set2.CharList)
-                set1.Add(c);
-            return set1;
+            var set = new SetOfChar();
+            foreach (var c in set1.CharList)
+                set.Add(c);
+            for (var i = 0; i != set2.CharList.Count; i++)
+                set.Add(set2.CharList[i]);
+            return set;
         }
 
         public static bool operator ==(SetOfChar set1, SetOfChar set2)
         {
-            return set1.CharList.Equals(set2.CharList);
+            if (set1.CharList.Count != set2.CharList.Count)
+                return false;
+            var trueOrFalse = false;
+            for (var i = 0; i != set1.CharList.Count; i++)
+            {
+                if (set1.CharList[i] == set2.CharList[i])
+                    trueOrFalse = true;
+                else
+                {
+                    trueOrFalse = false;
+                    break;
+                }
+            }
+            return trueOrFalse;
         }
 
         public static bool operator !=(SetOfChar set1, SetOfChar set2)
         {
-            return !set1.CharList.Equals(set2.CharList);
+            if (set1.CharList.Count != set2.CharList.Count)
+                return true;
+            var trueOrFalse = false;
+            for (var i = 0; i != set1.CharList.Count; i++)
+            {
+                if (set1.CharList[i] == set2.CharList[i])
+                    trueOrFalse = true;
+                else
+                {
+                    trueOrFalse = false;
+                    break;
+                }
+            }
+            return !trueOrFalse;
         }
 
         public override string ToString()
@@ -54,16 +89,74 @@ namespace Lab5
             return str;
         }
 
-        public string DisplayMenu()
+        public static string DisplayMenu()
         {
             Console.WriteLine("1. Ввести первое и второе множества.");
             Console.WriteLine("2. Объединить множества.");
             Console.WriteLine("3. Сравнить множества.");
             Console.WriteLine("4. Добавить элемент в множество.");
-            Console.WriteLine("5. Выход.");
+            Console.WriteLine("5. Вывести множество.");
+            Console.WriteLine("6. Выход.");
 
             var cki = Console.ReadKey(false);
             return cki.KeyChar.ToString();
         }
+    }
+
+    class TestSet
+    {
+        static void Main()
+        {
+            string userInput;
+            SetOfChar set1 = null, set2 = null;
+
+            do
+            {
+                userInput = SetOfChar.DisplayMenu();
+                switch (userInput)
+                {
+                    case "1":
+                        Console.Clear();
+                        Console.WriteLine("Введите первое и второе множества.");
+                        var chars1 = Console.ReadLine().Split(' ');
+                        var chars2 = Console.ReadLine().Split(' ');
+                        set1 = new SetOfChar(chars1);
+                        set2 = new SetOfChar(chars2);
+                        break;
+                    case "2":
+                        Console.Clear();
+                        Console.WriteLine(set1 + set2);
+                        break;
+                    case "3":
+                        Console.Clear();
+                        Console.WriteLine(set1 == set2 ? "Множества равны." : "Множества не равны.");
+                        break;
+                    case "4":
+                        Console.Clear();
+                        Console.WriteLine("Введите номер множества.");
+                        var num = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Введите символ.");
+                        var ch = Convert.ToChar(Console.ReadLine());
+                        if (num == 1)
+                            set1?.Add(ch);
+                        else
+                            set2?.Add(ch);
+                        break;
+                    case "5":
+                        Console.Clear();
+                        Console.WriteLine("Введите номер множества.");
+                        var num1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine(num1 == 1 ? set1 : set2);
+                        break;
+                    case "6":
+                        Console.Clear();
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Неверный пункт меню!");
+                        break;
+                }
+            } while (userInput != "6");
+        } 
     }
 }
