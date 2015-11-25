161776457c40dc64e781ef7c5065e4f151336d23
Ilshat Galimov <einnks@gmail.com>
2015-11-25 18:50:17 +0300

    2 constructors are added

--- Lab5/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Lab5
-{
-    class Program
-    {
-    }
-}
---
+++ Lab5/SetOfChar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab5
+{
+    class SetOfChar
+    {
+        protected List<char> CharList;
+
+        public SetOfChar(char ch)
+        {
+            CharList = new List<char> {ch};
+        }
+
+        public SetOfChar(string[] str)
+        {
+            CharList = new List<char>();
+            foreach (var s in str)
+                CharList.Add(Convert.ToChar(s));
+        }
+    }
+}
