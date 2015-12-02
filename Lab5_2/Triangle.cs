f9cffd7c00606c952dea0c6f1518a55174a4b14d
Ilshat Galimov <einnks@gmail.com>
2015-12-02 20:26:47 +0300

    Semiperimeter and Area methods are added

---
+++ Lab5_2/Triangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lab5_2
+{
+    class Triangle
+    {
+        private double _firstSide;
+        private double _secondSide;
+        private double _thirdSide;
+
+        public Triangle(double firstSide, double secondSide, double thirdSide)
+        {
+            _firstSide = firstSide;
+            _secondSide = secondSide;
+            _thirdSide = thirdSide;
+        }
+
+        public double SemiPerimeter() => (_firstSide + _secondSide + _thirdSide)/2;
+
+        public double Area() => SemiPerimeter()*(SemiPerimeter() - _firstSide)*(SemiPerimeter() - _secondSide)
+                                *(SemiPerimeter() - _thirdSide);
+    }
+}
